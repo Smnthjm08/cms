eac2bb3ad430a395c3a8d524c3e669dee6c70090
Smnthjm08 <sumanthjmm.04@gmail.com>
2024-12-20 22:22:24 +0530

    shadcn sidebar init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           showSpinner={false}
         />
         <Providers>
-          <Navbar />
+          {/* <Navbar /> */}
           <OfflineNotification />
           {children}
           <Toaster richColors />
